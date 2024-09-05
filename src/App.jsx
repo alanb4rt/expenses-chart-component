27be24104321d5c3a4829654190db8f14ac35d81
alanb4rt <al4n.b4rt@gmail.com>
2024-09-05 13:10:41 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div>
+        <p>My balance</p>
+        <p>$921.48</p>
+
+        <p>Spending - Last 7 days</p>
+
+        <p>mon</p>
+        <p>tue</p>
+        <p>wed</p>
+        <p>thu</p>
+        <p>fri</p>
+        <p>sat</p>
+        <p>sun</p>
+
+        <p>Total this month</p>
+        <p>$478.33</p>
+
+        <p>+2.4%</p>
+        <p>from last month</p>
+      </div>
+    </>
+  );
+}
