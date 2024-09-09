0bb85c14492a911dc0b6e5be7888f0a33756fe80
alanb4rt <al4n.b4rt@gmail.com>
2024-09-09 15:32:10 +0200

    change color of maxAount

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,6 +1,8 @@
 export default function BarChart({ day, amount, maxAmount }) {
   const amountPercentage = (amount / maxAmount) * 100;
 
+  const isMaxAmount = amount === maxAmount;
+
   return (
     <div className="flex-1 flex flex-col gap-1 text-center">
       <div className="h-full flex flex-col justify-end gap-1 w-full">
@@ -8,10 +10,14 @@ export default function BarChart({ day, amount, maxAmount }) {
           className="group relative w-full px-1 sm:px-2"
           style={{ height: `${amountPercentage.toFixed(2)}%` }}
         >
-          <div className="hidden group-hover:block absolute inset-x-0 -top-8 w-full text-sm text-white bg-[var(--dark-brown)] py-1 rounded">
+          <div className="hidden group-hover:block absolute inset-x-0 -top-8 w-full min-w-fit text-sm text-white py-1 bg-[var(--dark-brown)] rounded">
             ${amount}
           </div>
-          <div className="group-hover:opacity-60 cursor-pointer w-full h-full rounded bg-[var(--soft-red)]"></div>
+          <div
+            className={`group-hover:opacity-60 cursor-pointer w-full h-full rounded ${
+              isMaxAmount ? "bg-[var(--cyan)]" : "bg-[var(--soft-red)]"
+            }`}
+          ></div>
         </div>
       </div>
       <p className="text-[var(--medium-brown)]">{day}</p>
