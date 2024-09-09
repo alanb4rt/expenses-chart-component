22010358449522d337f8b27c383750e2dd27a447
alanb4rt <al4n.b4rt@gmail.com>
2024-09-09 15:59:36 +0200

    refacto height component

--- src/components/WeeklySpendingBarChart.jsx
+++ src/components/WeeklySpendingBarChart.jsx
@@ -5,7 +5,7 @@ export default function WeeklySpendingBarChart() {
   const maxAmount = Math.max(...data.map((item) => item.amount));
 
   return (
-    <div className="flex justify-between h-44 mt-12">
+    <div className="flex justify-between h-48 mt-12">
       {data.map((element, index) => (
         <BarChart
           key={index}
